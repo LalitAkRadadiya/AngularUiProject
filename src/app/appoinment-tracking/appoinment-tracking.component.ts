144281b537836a85dd0fd051b42169319a94db69
Lalit Radadiya <lalit.radadiya@gatewaygroup.com>
2021-03-18 13:24:16 +0530

    d

--- src/app/appoinment-tracking/appoinment-tracking.component.ts
+++ src/app/appoinment-tracking/appoinment-tracking.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgxSpinnerService } from 'ngx-spinner';
+
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { SharedService } from 'src/app/shared.service'
 @Component({
@@ -16,7 +18,9 @@ export class AppoinmentTrackingComponent implements OnInit {
   EditAppoinment : any;
   AppServiceList : any;
   PlanningList : any;
-  constructor(private service: SharedService, private router: ActivatedRoute) { }
+  constructor(private service: SharedService, private router: ActivatedRoute , public spinner : NgxSpinnerService) {
+  
+  }
 
   ModalTitle = "Apppoinment Service and Planning Details"
   ActivateAddEditAppoinmentComp = false;
@@ -31,6 +35,8 @@ export class AppoinmentTrackingComponent implements OnInit {
   get_service_planning(id: Number) {
     console.log('id', id)
     if (id != 0) {
+      
+    this.spinner.show();
       this.service.getEditAppoinmentList(id).subscribe(res => {
         this.EditAppoinment = res;
 
@@ -38,24 +44,30 @@ export class AppoinmentTrackingComponent implements OnInit {
         this.AppServiceList = this.EditAppoinment.appointmentServicesList;
         this.PlanningList = this.EditAppoinment.planningList;
 
+        this.spinner.hide();
         console.log('get service Planingn detail', res);
       });
+      
     }
   }
   ngOnInit(): void {
-    
     this.id = this.router.snapshot.params.id;
     console.log('id',this.id)
 
 
     if (this.id > 0) {
+      
+    this.spinner.show();
       this.service.getAppointmentById(this.id).subscribe(res => {    
         this.appoinmentData = res;
         this.orderStatus = this.appoinmentData.Status;
+        this.spinner.hide();
         console.log(this.appoinmentData);
+        
       });
     }
     
     this.MoreDetail();
+    
   }
 }
