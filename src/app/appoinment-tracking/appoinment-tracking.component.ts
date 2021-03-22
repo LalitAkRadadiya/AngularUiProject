781d18b6582ca4ba883f18c40c56a21e33f45832
Lalit Radadiya <lalit.radadiya@gatewaygroup.com>
2021-03-22 16:18:49 +0530

    tracker added

--- src/app/appoinment-tracking/appoinment-tracking.component.ts
+++ src/app/appoinment-tracking/appoinment-tracking.component.ts
@@ -4,6 +4,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { SharedService } from 'src/app/shared.service'
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-appoinment-tracking',
   templateUrl: './appoinment-tracking.component.html',
@@ -18,7 +19,36 @@ export class AppoinmentTrackingComponent implements OnInit {
   EditAppoinment : any;
   AppServiceList : any;
   PlanningList : any;
-  constructor(private service: SharedService, private router: ActivatedRoute , public spinner : NgxSpinnerService) {
+  Id: Number = 0;
+  FName!: string;
+  LName!: string;
+  MobileNo!: Number;
+  Email!: string;
+  City!: string;
+  Country!: string;
+  Model!: string;
+  Brand!: string;
+  LicencePlate!: string;
+  Status!: string;
+  TotalTime!: string;
+  TotalPrice!: string;
+  CreatedBy!: string;
+  UpdateBy!: string;
+
+
+  MechanicId: Number;
+  DealerId: Number;
+  ServiceId: Number;
+  CostType!: string;
+  SalesPart!: string;
+  Quantity!: string;
+  PricePerUnit!: string;
+
+
+  StartDate!: string;
+  EndDate!: string;
+  Duration!: string;
+  constructor(private toastr: ToastrService,private service: SharedService, private router: ActivatedRoute , public spinner : NgxSpinnerService) {
   
   }
 
@@ -40,6 +70,7 @@ export class AppoinmentTrackingComponent implements OnInit {
       this.service.getEditAppoinmentList(id).subscribe(res => {
         this.EditAppoinment = res;
 
+        this.loadServiceList(res.DealerId);
 
         this.AppServiceList = this.EditAppoinment.appointmentServicesList;
         this.PlanningList = this.EditAppoinment.planningList;
@@ -50,6 +81,7 @@ export class AppoinmentTrackingComponent implements OnInit {
       
     }
   }
+  
   ngOnInit(): void {
     this.id = this.router.snapshot.params.id;
     console.log('id',this.id)
@@ -70,4 +102,92 @@ export class AppoinmentTrackingComponent implements OnInit {
     this.MoreDetail();
     
   }
+
+
+  
+  DealerList: any = [];
+  ServiceList: any = [];
+  updatedStatus: any;
+
+  MechanicList: any = [];
+
+
+  serviceAppoinment: any;
+  currentAppoinmentServiceId: any;
+
+  displayServicePlanningList = true;
+  disappoinmentserviceubtton = true;
+
+  displanningubutton = false;
+
+  moreService_PlanningButton = false;
+
+
+  moreService_Planning() {
+    this.moreService_PlanningButton = false;
+    this.disappoinmentserviceubtton = true;
+
+  }
+
+  // AppServiceList: 
+  // PlanningList: 
+  
+  tempAppservice = false;
+  validationofappoinmentservice() {
+    if (!this.ServiceId) {
+      this.tempAppservice = true;
+    } else {
+      this.tempAppservice = false;
+    }
+  }
+  
+  loadServiceList(val: any) {
+    if (val != undefined) {
+
+      this.service.serviceDropdown(val).subscribe(data => {
+        console.log('load', data);
+        this.ServiceList = data;
+      });
+    }
+  }
+  AddAppoinmentService() {
+    this.validationofappoinmentservice();
+
+    if (!this.tempAppservice) {
+
+      var val = {
+        AppointmentId: this.id,
+        ServiceId: this.ServiceId,
+        CostType: 'FIX',
+        SalesPart: 50,
+        Quantity: this.Quantity,
+        PricePerUnit: 30,
+        CreatedBy: "Lalit"
+      };
+      // this.AppServiceList.push(val);
+      console.log('val cal', val);
+
+      this.service.addAppoinmentService(val).subscribe(res => {
+        console.log(res);
+        this.currentAppoinmentServiceId = res;
+        this.displanningubutton = true;
+        this.disappoinmentserviceubtton = false;
+        this.get_service_planning(val.AppointmentId);
+        this.toastr.success("Created Successfully", '', {
+          timeOut: 3000,
+        });
+      });
+
+    } else {
+      return false;
+    }
+  }
+  MechanicNotAvailble = false;
+
+  tempMechanicName = false;
+  tempstartDate = false;
+  tempEndDate = false;
+  validdate = false;
+
+
 }
